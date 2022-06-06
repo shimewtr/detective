1bf61203748356cc0314805312854ebe1ce0246e
ShimeWataru <shime@shimewtr.me>
2022-06-06 22:05:36 +0900

    Fix fetch members

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ class Detective():
         team_name = input("Please enter team name :")
         user_name = input("Please enter user name :")
 
-        client = Client(
+        self.client = Client(
             transport=RequestsHTTPTransport(
                 url = "https://api.github.com/graphql",
                 use_json = True,
@@ -22,65 +22,47 @@ class Detective():
             ),
             fetch_schema_from_transport=True,
         )
-        resp = client.execute(
-            gql("""
-                query($organization_name:String!, $team_name:String!){
-                    organization(login:$organization_name) {
-                        teams(first: 1, query:$team_name) {
-                            nodes {
-                                name
-                                members(first: 30) {
-                                    nodes {
-                                        login
-                                        contributionsCollection(
-                                            from: "2022-04-01T00:00:00+09:00"
-                                            to: "2022-05-25T23:59:59+09:00"
-                                        ) {
-                                            pullRequestContributions(last: 100, orderBy: {direction: ASC}) {
-                                                nodes {
-                                                    pullRequest {
-                                                        title
-                                                        url
-                                                        additions
-                                                        deletions
-                                                        state
-                                                        commits {
-                                                            totalCount
-                                                        }
-                                                    }
-                                                }
-                                            }
-                                            totalPullRequestContributions
-                                            totalCommitContributions
+        self.members = self.fetch_members()
+        print(self.members)
+
+    def fetch_members(self):
+        members = []
+        after = None
+        while True:
+            resp = self.client.execute(
+                gql("""
+                    query($organization_name:String!, $team_name:String!, $after:String){
+                        organization(login:$organization_name) {
+                            teams(first: 1, query:$team_name) {
+                                nodes {
+                                    name
+                                    members(first: 100 after:$after) {
+                                        pageInfo {
+                                            hasNextPage
+                                            endCursor
+                                        }
+                                        nodes {
+                                            login
                                         }
                                     }
                                 }
                             }
                         }
                     }
-                }
-            """),
-            variable_values={
-                "organization_name": organization_name,
-                "team_name": team_name,
-                "user_name": user_name
-            })
-
-        print(",".join(["name", "additions", "deletions", "merged_pull_requests", "commits"]))
-        for member in resp["organization"]["teams"]["nodes"][0]["members"]["nodes"]:
-            login = member["login"]
-            additions = 0
-            deletions = 0
-            merged_pull_requests = 0
-            commits = 0
-            for pull_request_contributeion in member["contributionsCollection"]["pullRequestContributions"]["nodes"]:
-                pull_request = pull_request_contributeion["pullRequest"]
-                if pull_request["state"] == "MERGED" or pull_request["state"] == "OPEN":
-                    additions += pull_request["additions"]
-                    deletions += pull_request["deletions"]
-                    merged_pull_requests += 1
-                    commits += pull_request["commits"]["totalCount"]
-            print(",".join([str(n) for n in [login, additions, deletions, merged_pull_requests, commits]]))
+                """),
+                variable_values={
+                    "organization_name": self.organization_name,
+                    "team_name": self.team_name,
+                    "after": after
+                })
+            page_info = resp["organization"]["teams"]["nodes"][0]["members"]["pageInfo"]
+            after = page_info["endCursor"]
+            has_next_page = page_info["hasNextPage"]
+            for member in resp["organization"]["teams"]["nodes"][0]["members"]["nodes"]:
+                members.append(member["login"])
+            if not has_next_page:
+                break
+        return members
 
 if __name__ == '__main__':
     Detective()
